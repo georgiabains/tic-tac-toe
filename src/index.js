c810f9a212955fa741a3e51dbd76d5f93a9f9f10
Georgia Bains <georgia.bains@mail.utoronto.ca>
2020-06-30 22:42:28 -0400

    Added coordinates to game board.

--- src/index.js
+++ src/index.js
@@ -25,16 +25,25 @@ class Board extends React.Component {
     return (
       <div>
         <div className="board-row">
+          <p></p>
+          <p>A</p>
+          <p>B</p>
+          <p>C</p>
+        </div>
+        <div className="board-row">
+          <p className="board-num">1</p>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
+          <p className="board-num">2</p>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
         <div className="board-row">
+          <p className="board-num">3</p>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
